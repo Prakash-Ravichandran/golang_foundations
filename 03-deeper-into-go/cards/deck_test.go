dd46f43b9dca11aa04d936a9b2086b34f93edc63
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2026-04-19 13:07:24 +0530

    add saveToFileAndReadFromFile

--- 03-deeper-into-go/cards/deck_test.go
+++ 03-deeper-into-go/cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	cards:= newDeck()
@@ -8,4 +11,29 @@ func TestNewDeck(t *testing.T) {
 	if len(cards) != 16 {
 		t.Errorf("Expected deck length of 16, but got %d", len(cards))
 	}
+
+	if cards[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be Ace of Spades, but got %s", cards[0])
+	}
+
+	if cards[len(cards)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card to be Four of Clubs, but got %s", cards[len(cards)-1])
+	}
+}
+
+func TestSaveToFileAndReadFromFile(t *testing.T) {
+   os.Remove("test_deck")
+
+	cards := newDeck()
+
+	cards.saveToFile("test_deck")
+
+
+	loadedCards := cards.readFromFile("test_deck")
+
+	if len(loadedCards) != 16 {
+		t.Errorf("Expected deck length of 16, but got %d", len(loadedCards))
+	}
+
+	os.Remove("test_deck")
 }
